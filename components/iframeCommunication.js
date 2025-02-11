a5b266fef1f0556e695d1cd33628f1f6182106cb
Bobby Kolev <bobbykolev@abv.bg>
2025-02-11 16:27:46 +0200

    Revert back the full page scroll logic + fix infinite scroll updates (#45)
    * Revert back to no internal scroll.
    
    * 0.1.1
    
    * Bump the update height threshold
    
    * Bump the update height threshold

--- components/iframeCommunication.js
+++ components/iframeCommunication.js
@@ -1,6 +1,8 @@
 import { useEffect, useRef } from 'react';
 import { useRouter } from 'next/router';
 
+const HEIGHT_DIFFERENCE_THRESHOLD = 130; // ~30 locally and 122 on Sandbox (Donna PC)
+
 const allowedOrigins = ['https://alkem.io', 'https://dev-alkem.io', 'https://acc-alkem.io', 'https://sandbox-alkem.io', 'http://localhost:3000'];
 const isOriginValid = (origin) => allowedOrigins.includes(origin);
 
@@ -40,14 +42,16 @@ const IframeCommunication = () => {
 
     const sendPageHeight = () => {
         const pageHeight = document.documentElement.scrollHeight || document.body.scrollHeight;
+        console.log('Scroll change new/old: ', pageHeight, lastHeight.current);
 
         // Only send if there's a meaningful difference in height
-        if (Math.abs(pageHeight - lastHeight.current) > 40) {
+        if (Math.abs(pageHeight - lastHeight.current) > HEIGHT_DIFFERENCE_THRESHOLD) {
             lastHeight.current = pageHeight;
 
             // Debounce the message to avoid excessive calls
             clearTimeout(debounceTimeout.current);
             debounceTimeout.current = setTimeout(() => {
+                console.log('Scroll change SENT: ', pageHeight, lastHeight.current);
                 sendMessageToParent({ type: SupportedMessageTypes.PageHeight, height: pageHeight });
             }, 50);
         }
@@ -55,9 +59,8 @@ const IframeCommunication = () => {
     
 
     useEffect(() => {
-        // Send initial page height and path
+        // Send path
         sendMessageToParent({ type: SupportedMessageTypes.PageChange, url: router.pathname });
-        sendPageHeight();
 
         // Observe changes to the body size
         const resizeObserver = new ResizeObserver(sendPageHeight);
