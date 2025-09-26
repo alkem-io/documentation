2ba22ef689b1b0a5f99f610493ab0630f359d78a
Donna <donna@alkem.io>
2025-09-26 15:34:35 +0200

    Embedding tutorials and making improvements

---
+++ components/TutorialSettings.js
@@ -0,0 +1,31 @@
+export default function TutorialSettings() {
+  return (
+    <div
+      style={{
+        position: 'relative',
+        width: '100%',
+        paddingTop: '40%',
+        paddingBottom: '5%',
+        height: 0,
+        overflow: 'hidden',
+      }}
+    >
+      <iframe
+        src="https://demo.arcade.software/Rbwhpk4zro3Uer61iQKL?embed&embed_mobile=tab&embed_desktop=inline&show_copy_link=true"
+        title="Arcade embed"
+        frameBorder="0"
+        loading="lazy"
+        allow="clipboard-write"
+        style={{
+          position: 'absolute',
+          top: 0,
+          left: 0,
+          width: '100%',
+          height: '100%',
+          border: 0,
+          colorScheme: 'light',
+        }}
+      />
+    </div>
+  );
+}
---
+++ components/TutorialSignupNL.js
@@ -0,0 +1,30 @@
+export default function TutorialSignupNL() {
+  return (
+    <div
+      style={{
+        position: 'relative',
+        width: '100%',
+        paddingTop: '40%',
+        height: 0,
+        overflow: 'hidden',
+      }}
+    >
+      <iframe
+        src="https://demo.arcade.software/jPkC1W2J9Ijr2QTHRwBN?embed&embed_mobile=tab&embed_desktop=inline&show_copy_link=true"
+        title="Arcade embed"
+        frameBorder="0"
+        loading="lazy"
+        allow="clipboard-write"
+        style={{
+          position: 'absolute',
+          top: 0,
+          left: 0,
+          width: '100%',
+          height: '100%',
+          border: 0,
+          colorScheme: 'light',
+        }}
+      />
+    </div>
+  );
+}
