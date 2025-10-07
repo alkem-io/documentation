be10111ab8e0e4761a524da2936ba6d2cf171d96
Donna <donna@alkem.io>
2025-10-07 12:15:41 +0200

    Translations september until 7th of October

---
+++ components/TutorialCommunity.js
@@ -0,0 +1,31 @@
+export default function TutorialCommunity() {
+  return (
+    <div
+      style={{
+        position: 'relative',
+        width: '100%',
+        paddingTop: '40%',
+        paddingBottom: '5%',
+        height: 0,
+        overflow: 'hidden',
+      }}
+    >
+      <iframe
+        src="https://demo.arcade.software/guBQToL8DWsnjCE7GLve?embed&embed_mobile=tab&embed_desktop=inline&show_copy_link=true"
+        title="How to set up your community"
+        frameBorder="0"
+        loading="lazy"
+        allow="clipboard-write"
+        style={{
+          position: 'absolute',
+          top: 0,
+          left: 0,
+          width: '100%',
+          height: '100%',
+          border: 0,
+          colorScheme: 'light',
+        }}
+      />
+    </div>
+  );
+}
