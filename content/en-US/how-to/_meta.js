39e8e2a1925602a44e96a25e0c6ee33bf6e13c41
Anne Klok <anne@alkem.io>
2026-04-23 14:39:03 +0200

    updatedhowtos

--- content/en-US/how-to/_meta.js
+++ content/en-US/how-to/_meta.js
@@ -1,9 +1,14 @@
 export default {
-  inviting: { title: 'Inviting People to a Space' },
-  'join-requests': { title: 'Manage Join Requests' },
-  'create-subspace': { title: 'Create a Subspace' },
-  whiteboards: { title: 'Whiteboards' },
-  'activate-community': { title: 'Activate the Community' },
-  'make-post-beautiful': { title: 'Make your Post look beautiful' },
+  'setup-free-space': { title: 'Set up your free Space' },
+  'design-space': { title: 'Design your Space' },
+  'design-subspaces': { title: 'Design your Subspaces' },
+  'onboard-members': { title: 'Onboard your first members & manage join requests' },
+  'activate-community': { title: 'Activate your community' },
+  'reuse-templates': { title: 'Reuse your work through templates' },
+  'collaboration-tools': { title: "Use Alkemio's collaboration tools" },
+  'template-packs': { title: 'Use template packs from other organisations' },
+  'change-email': { title: 'Change your email address' },
+  'report-bug': { title: 'Raise a bug / flag an issue' },
+  whiteboards: { title: 'Use Excalidraw whiteboards' },
   tutorials: { title: 'Tutorials' }
 }
