95b60ddd90b9510e39a8cc03153a5e67786868d7
Anne Klok <anne@alkem.io>
2026-07-01 12:51:06 +0200

    featuresupdate

--- content/en-US/features/_meta.js
+++ content/en-US/features/_meta.js
@@ -1,6 +1,9 @@
 export default {
   search: { title: 'Search' },
   posts: { title: 'Posts' },
+  documents: { title: 'Documents' },
+  'direct-messages': { title: 'Direct Messages' },
+  'guidance-chat': { title: 'Guidance Chat' },
   iframe: { title: 'iFrame' },
   videocall: { title: 'Video Call' },
   'virtual-contributors': { title: 'Virtual Contributors' },
--- content/nl-NL/features/_meta.js
+++ content/nl-NL/features/_meta.js
@@ -1,6 +1,9 @@
 export default {
   search: { title: 'Zoeken' },
   posts: { title: 'Posts' },
+  documents: { title: 'Documenten' },
+  'direct-messages': { title: 'Directe Berichten' },
+  'guidance-chat': { title: 'Guidance Chat' },
   iframe: { title: 'iFrame' },
   videocall: { title: 'Videogesprek' },
   'virtual-contributors': { title: 'Virtual Contributors' },
